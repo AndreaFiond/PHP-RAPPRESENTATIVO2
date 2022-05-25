dac903b5dc643a644f5e7d3548b3cc2d1732a202
AndreaFiond <104151446+AndreaFiond@users.noreply.github.com>
2022-05-25 18:24:00 +0200

    Fix e completamento inserisci.php
    Completamento funzionalità pagina

--- inserisci.php
+++ inserisci.php
@@ -56,7 +56,34 @@ PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 				}
 			}
 			</style>
+			
+			<style type="text/css">
+			#contenitore{
+				background-color:beige;
+				margin-left:auto;
+				margin-right:auto;
+				display:flex;       
+				justify-content:space-around;
+				align-items:center;
+				border-style:solid;
+				border-color:blue;
+				font-size:25px;
+				height:30%;
+				width:60%;
+			}
+		</style>
 </head>
+<script type="text/javascript">
+			function formvalidator(){
+				var usr = document.forms['userform']['username'].value; //assegnamo ad usr il valore di username
+				var pwd= document.forms['userform']['password'].value;
+				if(usr == null || pwd == null || usr == "" || pwd == ""){
+					alert ("Dati mancanti");
+					return false;
+				}
+				return true;
+			}
+		</script>
 
 <body>
 	<div id="header">
@@ -64,5 +91,27 @@ PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"
 			<li><a href="Logout.php" title="Logout">Logout</a></li>
 		</ul>
 	</div>
+	<h1 style="margin-top:5%;display:flex;justify-content:center;color:blue; font-family:Arial">INSERIRE NUOVO TELEFONO</h1>
+		<div id="contenitore">
+			<form name="userform" action="inserisci.php" method="post" onsubmit="return formvalidator()">
+				<p>Modello: <input type="text" name="modello"></p>
+				<p>Prezzo: <input type="text" name="prezzo"></p>
+				<p style="display:flex; justify-content:center"><input type="submit" name="inserisci" value="inserisci"></p>
+			</form>
+	<?php
+		if(isset($_POST['inserisci'])){ //se abbiamo compilato la form
+		$modello=$_POST['modello'];
+		$prezzo=$_POST['prezzo'];
+		require_once("connection.php");
+		$sql= "INSERT INTO Telefono(nome, prezzo) VALUES ('$modello', '$prezzo')";	
+		$queryresult= mysqli_query($connection,$sql); /*mandiamo la query al dbs*/
+			if($queryresult){ /*se la query da un risultato valido*/ 
+					echo "<h2 style=\"color:green\">inserimento riuscito</h2>";
+				}	
+				else {
+					echo "<h2 style=\"color:red\">errore inserimento</h2>";
+				}
+		}
+	?>	
 	</body>
 	</html>
